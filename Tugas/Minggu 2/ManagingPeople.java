f2e2f1aa97843c5bb9cace3641947b895136abd9
chandrapramudya <candrapramudya5@gmail.com>
2021-03-16 20:19:22 -0700

    Create ManagingPeople.java

---
+++ Tugas/Minggu 2/ManagingPeople.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package managingpeople;
+
+/**
+ *
+ * @author user
+ */
+public class ManagingPeople {
+
+String name;
+    int age;
+
+    public ManagingPeople(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
