b9cf2a3612648b6088c3da6c128d7cd4458f3deb
chandrapramudya <candrapramudya5@gmail.com>
2021-03-16 20:19:20 -0700

    Create KesalahanDeklarasi.java

---
+++ Tugas/Minggu 2/KesalahanDeklarasi.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kesalahan.deklarasi;
+
+/**
+ *
+ * @author user
+ */
+public class KesalahanDeklarasi {
+
+    boolean gameOver = false; //Tidak terdapat kesalahan dalam pendeklarasian variable.
+    int students = 50, classes = 3; //Tidak terdapat kesalahan dalam pendeklarasian variable.
+    double sales_tax; //Tidak terdapat kesalahan dalam pendeklarasian variable.
+    short number1; //Tidak terdapat kesalahan dalam pendeklarasian variable.
+    
+    int 2beOrNot2be; 
+    // Terdapat kesalahan dalam pendeklarasian variable dimana nama variable tidak diperbolehkan dimulai dengan angka atau bilangan.
+    float price index;
+    // Terdapat kesalahan dalam pendeklarasian variable dimana nama variable tidak diperbolehkan menggunakan spasi.
+    double lastYear'Price;
+    // Terdapat kesalahan dalam pendeklarasian variable dimana nama variable tidak boleh menggunakan simbol ditengah nama variable, kecuali simbol underscore(_).
+    long class;
+    // Terdapat kesalahan dalam pendeklarasian variable dimana nama variable tersebut merupakan reserved word atau keyword java yang telah tertanam di dalam program java.
+    
+}
