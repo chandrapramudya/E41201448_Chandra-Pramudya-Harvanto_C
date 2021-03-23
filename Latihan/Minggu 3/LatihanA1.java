5c0fec6b2f70214d7f02405c26ea416d66be9148
chandrapramudya <candrapramudya5@gmail.com>
2021-03-23 10:24:26 -0700

    Create LatihanA1.java

---
+++ Latihan/Minggu 3/LatihanA1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan.a1;
+
+/**
+ *
+ * @author user
+ */
+public class LatihanA1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int[] angka = {3, 7, 8, 1, 10, 20, 35};
+        
+        System.out.println("Jumlah elemen array angka = "+angka.length);
+    }
+    
+}
