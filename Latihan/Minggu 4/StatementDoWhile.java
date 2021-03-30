3275e519d93d6d1f43d3ef1a4ed70b95cadf63ed
chandrapramudya <candrapramudya5@gmail.com>
2021-03-29 17:55:30 -0700

    Create StatementDoWhile.java

---
+++ Latihan/Minggu 4/StatementDoWhile.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+public class StatementDoWhile {
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //membuat variabel
+        int i = 0;
+        
+        do {
+            System.out.println("Perulangan ke-" + i);
+            i++;
+        } while ( i <= 10);
+        
+    }
+}
