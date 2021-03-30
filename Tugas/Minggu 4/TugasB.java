8dc18e42c4738c13268a38d594f5557e1579d2c5
chandrapramudya <candrapramudya5@gmail.com>
2021-03-29 17:55:49 -0700

    Create TugasB.java

---
+++ Tugas/Minggu 4/TugasB.java
@@ -0,0 +1,29 @@
+package tugasa;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+public class TugasB {
+    
+    public static void main(String[] args) {
+        // Deklarasi Variabel
+        int i = 1;
+        System.out.println("                  Do__While                      ");
+        System.out.println("             Bilangan Kelipatan 2                ");
+        System.out.println("                   (1-100)                       ");
+        System.out.println("=================================================");
+        //Coding
+        do {
+            System.out.print(i+" " );
+            i = i * 2;
+            System.out.print(" ");
+        } while (i<=100);
+    }
+}
