d69c0f649d3dad820737b285a796b7e4dc70703d
chandrapramudya <candrapramudya5@gmail.com>
2021-03-29 17:55:42 -0700

    Create StatementWhile.java

---
+++ Latihan/Minggu 4/StatementWhile.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+public class StatementWhile {
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int i = 0;
+        
+        while (i <= 10) {
+            //blok kode yang akan diulang
+            System.out.println("Perulangan ke-" + i);
+            //increment nilai
+            i++;
+        } 
+    }
+}
